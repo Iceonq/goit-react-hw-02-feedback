4bfee6165b3a61ec36c11ab75b70910f50eece95
Iceonq <bartek.sosnowicz@gmail.com>
2023-03-19 15:31:29 +0100

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Please leave feedback</h1>
+        <button>Good</button>
+        <button>Neutral</button>
+        <button>Bad</button>
+        <h1>Statistics</h1>
+        <p>Good: {this.state.good}</p>
+        <p>Neutral: {this.state.neutral}</p>
+        <p>Bad: {this.state.bad}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
